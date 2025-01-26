da629bf029ff343f719542409b95ae4777d0bd7a
Sneha-Dutt <snehagdutt@gmail.com>
2025-01-26 12:51:22 +0530

    Add test

---
+++ addNumbers.cpp
@@ -0,0 +1,5 @@
+#include "addNumbers.h"
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "addNumbers.h"
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << add(num1, num2) << std::endl;
+
+    int array[5];
+    return array[6];
+    //return 0;
+}
