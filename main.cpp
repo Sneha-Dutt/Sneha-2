7f105d6fd8cb145a0753eda474b49c3a93b1d47f
Sneha-Dutt <snehagdutt@gmail.com>
2025-01-26 16:10:06 +0530

    Show unit test error message

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     int num2 = 10;
     std::cout << "The sum of " << num1 << " and " << num2 << " is: " << add(num1, num2) << std::endl;
 
-    int array[5];
-    return array[6];
-    //return 0;
+    //int array[5];
+    //return array[6];
+    return 0;
 }
